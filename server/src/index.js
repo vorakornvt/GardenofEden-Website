6e7d667f5ec6da58d50fbf22e991ecc616674052
vorakornvt <vorakorn.vt@gmail.com>
2025-05-09 13:37:30 +1000

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -1,20 +1,20 @@
 // IMPORTED SERVER MODULES
 // External modules
-require('dotenv').config();
-const express = require('express');
-const morgan = require('morgan');
-const fileUpload = require('express-fileupload');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const fileUpload = require("express-fileupload");
+const cors = require("cors");
 const helmet = require("helmet");
 
 // Local modules
-const config = require('./config/config'); 
-const ApiError = require('./utilities/ApiError');
-const apiErrorHandler = require('./middleware/apiErrorHandler');
-const routes = require('./routes/routes');
-const { dbPing } = require('./config/db');
-const corsOptions = require('./config/corsOptions');
-const debugStartup = require('debug')('app:startup');
+const config = require("./config/config");
+const ApiError = require("./utilities/ApiError");
+const apiErrorHandler = require("./middleware/apiErrorHandler");
+const routes = require("./routes/routes");
+const connectDB = require("./config/db");
+const corsOptions = require("./config/corsOptions");
+const debugStartup = require("debug")("app:startup");
 
 // Initialise application using express
 const app = express();
@@ -23,21 +23,21 @@ const app = express();
 // HTTP Header-setter security & CORS
 app.use(helmet());
 app.use(cors(corsOptions));
-debugStartup('Helmet & CORS Pre-Flight requests enabled');
+debugStartup("Helmet & CORS Pre-Flight requests enabled");
 
 // POST request parsing middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-debugStartup('POST parsing middleware enabled for JSON/URL');
+debugStartup("POST parsing middleware enabled for JSON/URL");
 
 // *NEW* - File parsing middleware
 app.use(fileUpload({ createParentPath: true }));
 
 // Cycle our requests through morgan to track our queries
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
 // Main routing middleware function
-app.use('/api', routes());
+app.use("/api", routes());
 
 // Not Found Route
 app.use((req, res, next) => {
@@ -48,9 +48,8 @@ app.use((req, res, next) => {
 app.use(apiErrorHandler);
 
 // Ping DB & Set Port
-dbPing.then(() => {
-  app.listen(
-    config.port, 
-    () => console.log(`Server is running on port: ${config.port}`)
-  );
+connectDB().then(() => {
+  app.listen(config.port, () => {
+    console.log(`🚀 Server running on port: ${config.port}`);
+  });
 });
